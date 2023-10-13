77e9b97e88787624bfd47d18c09d1ab9118a5dc1
DenSan83 <dennis.santillan83@gmail.com>
2023-10-13 18:39:34 -0400

    FIX rename test

--- src/test/java/com/devdensan/mercadona/ProductModelAppTests.java
+++ src/test/java/com/devdensan/mercadona/ProductModelAppTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class ModelLayerAppTests {
+class ProductModelAppTests {
 
 	@Autowired
 	private MockMvc mockMvc;
