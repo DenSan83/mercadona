5a63e297ad3e25b80b675b8f28d057a8fbad8afe
DenSan83 <dennis.santillan83@gmail.com>
2023-10-12 21:15:19 -0400

    ADD test for catalogue filtered via url

--- src/test/java/com/devdensan/mercadona/ModelLayerAppTests.java
+++ src/test/java/com/devdensan/mercadona/ModelLayerAppTests.java
@@ -4,12 +4,26 @@ import com.devdensan.mercadona.model.Category;
 import com.devdensan.mercadona.model.Product;
 import com.devdensan.mercadona.model.Promotion;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class ModelLayerAppTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void createCategory() {
@@ -66,6 +80,22 @@ class ModelLayerAppTests {
 		String expected = "Promotion{promotionId=0, discountPercentage=10"+
 				", startDate=2023-01-01, endDate=2023-12-31}";
 		assertEquals(expected, result);
+	}
+
+	@Test
+	public void filterCategories() throws Exception {
+		// given
+		String categorySlug = "gateaux";
+
+		// when
+		String responseJson = mockMvc.perform(MockMvcRequestBuilders.get("/").param("category", categorySlug))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		String expectedJson = "[{\"productId\":6,\"productName\":\"Chabrior\",\"description\":\"GÃ¢teaux Snack'Lait cacao - Les 10 gÃ¢teaux de 42 g\""
+				+ ",\"image\":\"soda-field.png\",\"price\":1.94,\"category\":{\"categoryId\":2,\"categoryName\":\"GÃ¢teaux\",\"slug\":\"gateaux\"},\"promotion\":null}]";
+
+		// then
+		assertEquals(expectedJson, responseJson);
 	}
 
 
