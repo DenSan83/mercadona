e466e289ae88c168996cd1bb1109523225caed8d
DenSan83 <dennis.santillan83@gmail.com>
2023-10-13 18:54:42 -0400

    ADD User and Role entities and tests

---
+++ src/main/java/com/devdensan/mercadona/model/User.java
@@ -0,0 +1,95 @@
+package com.devdensan.mercadona.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "email", nullable = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String userName, String password, Role role, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    public User(int userId, String userName, String password, Role role, String email) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/devdensan/mercadona/UserModelAppTests.java
@@ -0,0 +1,52 @@
+package com.devdensan.mercadona;
+
+import com.devdensan.mercadona.model.Category;
+import com.devdensan.mercadona.model.Role;
+import com.devdensan.mercadona.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserModelAppTests {
+
+	@Test
+	void createRole() {
+		// given
+		String roleName = "ADMIN";
+
+		// when
+		Role role = new Role(roleName);
+		String result = role.toString();
+
+		// then
+		String expected = "Role{roleId=0, roleName='ADMIN'}";
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void createUser() {
+		// given
+		String userName = "main_user";
+		String password = "123456";
+		String email = "123456@devdensan.com";
+		Role role = new Role("ADMIN");
+
+		// when
+		User user = new User(userName, password, role, email);
+		String result = user.toString();
+
+		// then
+		String expected = "User{userId=0, userName='main_user', password='123456', role=Role{roleId=0, roleName='ADMIN'}, email='123456@devdensan.com'}";
+		assertEquals(expected, result);
+	}
+
+
+}
