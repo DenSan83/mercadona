f55ae1a33007c563c18eb0aaa266e28d3c70af64
DenSan83 <dennis.santillan83@gmail.com>
2023-11-12 14:33:14 -0500

    ADD logout route

--- src/main/java/com/devdensan/mercadona/config/SecurityConfiguration.java
+++ src/main/java/com/devdensan/mercadona/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +30,16 @@ public class SecurityConfiguration {
                 .anyRequest().permitAll()
             .and()
                 .formLogin()
-                .loginPage("/login").permitAll();
+                .loginPage("/login")
+                .successHandler((request, response, authentication) -> {
+                    response.sendRedirect("/admin");
+                })
+                .permitAll()
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+        ;
 
         return http.build();
     }
